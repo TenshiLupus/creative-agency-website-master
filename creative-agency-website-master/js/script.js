d8820b462027e080fabd6d616c87f7a744f67117
Tenshilupus <tenshilupus@hotmail.com>
2021-10-10 23:33:58 +0200

    css email icon position fix

--- creative-agency-website-master/js/script.js
+++ creative-agency-website-master/js/script.js
@@ -1,7 +1,7 @@
-const toogle = document.querySelector('.toggle')
-const navigation = document.querySelector('navigation')
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-})
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
 
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
